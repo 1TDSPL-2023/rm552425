bb105cbc166a63e408b1bc088fd9480da3e8a0f4
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-05-04 18:20:56 -0300

    removendo, adicionando e procurando elementos no array

--- js/script.js
+++ js/script.js
@@ -6,21 +6,17 @@
     //Os métodos floor, round e ceil arredondam os números passados
     //O método random retorna um número entre 0 e 1. Ex: o.1233.
     
-//let r = "";
-    
+//let r = "";    
 //let g = "";
-    
 //let b = "";
 
     
 //r = Math.round(Math.random()*255);
-    
 //g = Math.round(Math.random()*255);
-    
 //b = Math.round(Math.random()*255);
     
 //h2Element.setAttribute("style", "color:rgb("+r+","+g+","+b+");");  //VERSÃO MENOS LIMPA
-    
+
 //h2Element.setAttribute("style", `color:rgb(${r},${g},${b});`);  //VERSÃO MAIS LIMPA; QUANDO CLICA, MUDA A COR ALEATORIAMENTE
     
 //h2Element.textContent = "Novo título"; //QUANDO CLICA NO TÍTULO, MUDA O TEXTO
@@ -52,3 +48,65 @@
 //OUTRO EXEMPLO:
 //let fruta = "melão";
 //console.log(fruta);
+
+
+
+
+
+//ARRAYS
+//O índice sempre começa do 0
+
+//let frutas = ["uva", "pêra", "melância"]
+    //console.log(frutas[0]);
+    //console.log(frutas[1]);
+    //console.log(frutas[2]);
+    //console.log("Tamanho do ARRAY: " + frutas.length);
+
+
+
+
+
+//ADICIONANDO UM NOVO ELEMENTO NO FINAL DO ARRAY
+
+//frutas.push("banana");
+//frutas.push("laranja");
+    //console.log(frutas);
+    //console.table(frutas);  //PARA EXIBIR EM FORMA DE LISTA
+
+
+
+//ADICIONANDO UM NOVO ELEMENTOS NO COMEÇO DO ARRAY
+
+ //frutas.unshift("maracujá");
+ //frutas.unshift("kiwi");
+    //console.log(frutas);
+    //console.table(frutas);
+
+
+
+//REMOVENDO UM ELEMENTO NO FINAL DO ARRAY
+
+//frutas.pop();
+//console.table(frutas);
+
+
+
+//REMOVENDO UM ELEMENTO NO COMEÇO DO ARRAY
+
+//frutas.shift();
+//console.table(frutas);
+
+
+
+//REALIZANDO UMA BUSCA DE UM ITEM PARA SABER O INDÍCE
+
+//let indice = frutas.indexOf("pêra");
+//console.log(frutas[indice]);
+
+
+
+//REMOVENDO ITENS ATRAVÉS DO ÍNDICE (indice, quantidade)
+
+//let indice = frutas.indexOf("pêra");
+//frutas.splice(indice,1);
+//console.table(frutas);
